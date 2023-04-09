3e79bce40f5c9e04d068b80637d616f13c4e95b0
kkb75281 <115064300+sabbunsopum@users.noreply.github.com>
2023-04-09 21:02:59 +0900

    2023.04.09

--- src/components/LoadingPage.vue
+++ src/components/LoadingPage.vue
@@ -2,28 +2,88 @@
 main
     #water(src="../assets/img/Asset3.png")
     img#bgItems(src="../assets/img/candle.png")
-    #showAnswers
+    #content
+        #showAnswers
+    #fixedAnswers
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue';
 import { skapi } from '@/main';
 
-let answers = ref([]);
+let answers = [];
 
 skapi.getRecords({
     table: {
         name: 'uploadAnswer',
     }
 }).then(result=>{
-    let answersArr = result.list;
-    let currentAnsLen = 0;
-    let prevAnsLen = 0;
+    for(let i =0; i<result.list.length; i++) {
+        let answer = result.list[i].data.answer;
+        answers.push(answer);
+    }
     let showAnswers = document.getElementById('showAnswers');
+    let fixedAnswers = document.getElementById('fixedAnswers');
+    let newObj = [];
+    let answerTop = new Array(answers.length);
+    let WIDTH = 300;
+    let HEIGHT = 30;
+    let DRAWTIME = 1400;
+    let DOWNTIME = 700;
+    let idx = 0;
 
-    setInterval(() => {
-        currentAnsLen = answersArr.length;
-    })
+    for (let i = 0; i < answerTop.length; i++) {
+        answerTop[i] = 0;
+    }
+
+    let drawInterval = setInterval(function () {
+        let leftWidth = Math.round(Math.random() * (window.innerWidth - WIDTH));
+        let answerDiv = document.createElement("div");
+        
+        answerDiv.classList.add("answer");
+        answerDiv.style.width = WIDTH + "px";
+        answerDiv.style.height = HEIGHT + "px";
+        answerDiv.style.position = "absolute";
+        answerDiv.style.textAlign = "center";
+        answerDiv.innerHTML = answers[idx++];
+        showAnswers.appendChild(answerDiv);
+
+        if (leftWidth + WIDTH >= showAnswers.offsetWidth) {
+            answerDiv.style.left = (leftWidth - WIDTH) + "px";
+        } else {
+            answerDiv.style.left = leftWidth + "px";
+        }
+        console.log(leftWidth, answerDiv.style.left)
+        
+        newObj.push(answerDiv);
+
+        if (newObj.length === answers.length) {
+            clearInterval(drawInterval);
+        }
+    }, DRAWTIME);
+
+    function downAnswer() {
+        setInterval(function () {
+            for (let i = 0; i < answers.length; i++) {
+                if (i < newObj.length) {
+                    newObj[i].style.top = answerTop[i] + "px";
+                    answerTop[i] += 30;
+                    // 글자의 범위가 경계(바닥) 바깥으로 나갔을 경우 제거
+                    if (answerTop[i] + HEIGHT >= showAnswers.offsetHeight/1.5) {
+                        if (showAnswers.contains(newObj[i])) {
+                            showAnswers.removeChild(newObj[i]);
+                            fixedAnswers.appendChild(newObj[i]);
+                            fixedAnswers.style.writingMode= 'vertical-lr';
+                            newObj[i].style.left = 0 + "px";
+                            answerTop[i] = 0;
+                        }
+                    }
+                }
+            }
+        }, DOWNTIME);
+    }    
+    downAnswer();
+
 
     
 
@@ -31,34 +91,19 @@ skapi.getRecords({
 
 onMounted(() => {
     // 물 색깔 변경
-    let changeWaterImgs = ['src/assets/img/water1.png', 'src/assets/img/water2.png'];
     let waterImg = document.getElementById("water");
 
     function changeWater(){
-        let imgNum = Math.floor(Math.random() * 2);
-        
-        waterImg.style.background = 'url('+changeWaterImgs[imgNum]+')';
-        waterImg.style.backgroundPosition = 'bottom';
-        waterImg.style.backgroundSize = 'cover';
-
-        waterImg.style.transitionProperty = 'opacity';
-        waterImg.style.transitionDuration = '1s';
-        // waterImg.style.transition-timing-function: linear;
-        // waterImg.style.transition-delay: .5s; /* 500밀리초 */
-
-        
-
-        setTimeout(changeWater,1000);
+        waterImg.classList.toggle('change');
+        setTimeout(changeWater,2000);
     }
     changeWater();
 
     function Mobile() { return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent); }
     if (Mobile()) {
-
-    } else {
-
+        let fixedAnswers = document.getElementById('fixedAnswers');
+        fixedAnswers.remove();
     }
-
 })
 </script>
 
@@ -76,6 +121,16 @@ main {
         width: 100vw;
         height: 100vh;
         opacity: 0.5;
+        transition: all 1s;
+        background: url(@/assets/img/water1.png) no-repeat;
+        background-position: bottom;
+        background-size: cover;
+
+        &.change {
+            background: url(@/assets/img/water2.png) no-repeat;
+            background-position: bottom;
+            background-size: cover;
+        }
     }
 
     #bgItems {
@@ -87,6 +142,40 @@ main {
         transform: translate(-50%, -50%);
         z-index: 999;
         object-fit: cover;
+    }
+
+    #content {
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+
+        #showAnswers {
+            width: 100%;
+            height: 100%;
+            position: relative;
+    
+            .answer {
+                color: #000;
+                font-size: 24px;
+                font-weight: 400;
+            }
+        }
+    }
+
+    #fixedAnswers {
+        position: absolute;
+        left: 40px;
+        top: 40px;
+        width: calc(100% - 80px);
+        height: calc(100% - 80px);
+
+        .answer {
+            color: #000;
+            font-size: 24px;
+            font-weight: 400;
+        }
     }
 }
 </style>
